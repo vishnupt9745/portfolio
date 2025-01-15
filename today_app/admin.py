6a4d1c2909871508f58f47948c3ea79da9c1780a
vishnu <vishnupt527@gmail.com>
2025-01-15 11:44:32 +0530

    Your message

--- today_app/admin.py
+++ today_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from today_app import models
+
 # Register your models here.
+admin.site.register(models.student)
+admin.site.register(models.mark)
--- today_app/models.py
+++ today_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class student(models.Model):
+    name=models.CharField(max_length=20)
+    email=models.EmailField()
+    student_id=models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+#foreinkey
+
+class mark(models.Model):
+    student=models.ForeignKey('student',on_delete=models.CASCADE)
+    total_mark=models.CharField(max_length=3)
+    grade=models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.grade
+
